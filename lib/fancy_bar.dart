df438eda87a766105f223cab34adba245f81412d
Leo Elstin <leoelstin@gmail.com>
2020-03-18 23:14:03 +0530

    Added Fancy Animation V2

--- lib/fancy_bar.dart
+++ lib/fancy_bar.dart
@@ -2,15 +2,19 @@ library fancy_bar;
 
 import 'package:flutter/material.dart';
 
+enum FancyType { FancyV1, FancyV2 }
+
 class FancyBottomBar extends StatefulWidget {
   final List<FancyItem> items;
   final selectedIndex;
   final ValueChanged<int> onItemSelected;
+  final FancyType type;
 
   FancyBottomBar({
     this.items,
     this.selectedIndex,
     @required this.onItemSelected,
+    this.type,
   }) {
     assert(items != null);
     assert(items.length >= 2 && items.length <= 4);
@@ -34,7 +38,6 @@ class _FancyBottomBarState extends State<FancyBottomBar> {
   Widget build(BuildContext context) {
     return Material(
       child: BottomAppBar(
-//        shape: CircularNotchedRectangle(),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: widget.items.map((_item) {
@@ -49,6 +52,7 @@ class _FancyBottomBarState extends State<FancyBottomBar> {
                 item: item,
                 isSelected: index == pos,
                 length: widget.items.length,
+                type: widget?.type,
               ),
             );
           }).toList(),
@@ -62,34 +66,112 @@ class _ItemVWidget extends StatelessWidget {
   final FancyItem item;
   final bool isSelected;
   final int length;
+  final FancyType type;
 
-  const _ItemVWidget({Key key, this.item, this.isSelected, this.length})
+  const _ItemVWidget(
+      {Key key, this.item, this.isSelected, this.length, this.type})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: MediaQuery.of(context).size.width / length,
-        height: kBottomNavigationBarHeight,
-        alignment: Alignment.center,
-        child: AnimatedSwitcher(
-          transitionBuilder: (Widget child, Animation<double> animation) {
-            return ScaleTransition(
-              scale: animation,
-              child: child,
-            );
-          },
-          duration: Duration(milliseconds: 250),
-          child: isSelected
-              ? Text(
-                  '${item.title}',
-                  style: TextStyle(
-                      color: item.textColor ?? Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16),
-                )
-              : item.icon,
-        ));
+    switch (type) {
+      case FancyType.FancyV1:
+        {
+          return Container(
+            width: MediaQuery.of(context).size.width / length,
+            height: kBottomNavigationBarHeight,
+            alignment: Alignment.center,
+            child: AnimatedSwitcher(
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return ScaleTransition(
+                  scale: animation,
+                  child: child,
+                );
+              },
+              duration: Duration(milliseconds: 250),
+              child: isSelected
+                  ? Text(
+                '${item.title}',
+                style: TextStyle(
+                    color:
+                    item.textColor ?? Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              )
+                  : item.icon,
+            ),
+          );
+        }
+
+      case FancyType.FancyV2:
+        {
+          return Container(
+            width: MediaQuery.of(context).size.width / length,
+            height: kBottomNavigationBarHeight,
+            child: AnimatedContainer(
+              height: 0,
+              color: Colors.white,
+              duration: Duration(milliseconds: 300),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.center,
+                    child: item.icon,
+                  ),
+                  AnimatedContainer(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: isSelected ? item.textColor : Colors.transparent,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(24),
+                        bottomRight: Radius.circular(24),
+                        topLeft: Radius.circular(4),
+                        topRight: Radius.circular(4),
+                      ),
+                    ),
+                    duration: Duration(milliseconds: 300),
+                    height: isSelected ? kToolbarHeight : 24,
+                    child: Text(
+                      '${item.title}',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      default:
+        {
+          return Container(
+            width: MediaQuery.of(context).size.width / length,
+            height: kBottomNavigationBarHeight,
+            alignment: Alignment.center,
+            child: AnimatedSwitcher(
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return ScaleTransition(
+                  scale: animation,
+                  child: child,
+                );
+              },
+              duration: Duration(milliseconds: 250),
+              child: isSelected
+                  ? Text(
+                '${item.title}',
+                style: TextStyle(
+                    color:
+                    item.textColor ?? Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              )
+                  : item.icon,
+            ),
+          );
+        }
+    }
   }
 }
 
@@ -97,10 +179,12 @@ class FancyItem {
   Color textColor;
   String title;
   Widget icon;
+  FancyType type;
 
   FancyItem({
     @required this.textColor,
     @required this.title,
     @required this.icon,
+    this.type,
   });
 }

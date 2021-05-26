36a990cdc91666d1aff81947f05e1bd3c46182e4
ElMubark A.Elwahab Hassan <Mobark.k11@outlook.com>
2021-05-26 13:12:38 +0200

    migrate to nullsafety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, this.title}) : super(key: key);
 
-  final String title;
+  final String? title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title!),
       ),
       bottomNavigationBar: FancyBottomBar(
         items: [
